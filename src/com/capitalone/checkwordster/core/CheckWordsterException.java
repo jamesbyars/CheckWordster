8903129a1879ffa99bfa1bf30f6d4384e248810c
James Byars <jamesbyars@g.harvard.edu>
2017-03-14 17:19:54 -0500

    Remove old capitalone package name

--- src/com/capitalone/checkwordster/core/CheckWordsterException.java
+++
@@ -1,6 +0,0 @@
-package com.capitalone.checkwordster.core;
-
-public class CheckWordsterException extends Exception {
-    public CheckWordsterException() { super(); }
-    public CheckWordsterException(String message) { super(message); }
-}
