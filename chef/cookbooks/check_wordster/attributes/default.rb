04a363ef6ccbda509e116b9177712b5158595fae
James Byars <jamesbyars@g.harvard.edu>
2017-03-16 13:42:23 -0500

    Parameterize service name and ignore logs

--- chef/cookbooks/check_wordster/attributes/default.rb
+++ chef/cookbooks/check_wordster/attributes/default.rb
@@ -1,4 +1,5 @@
 # Database configs
+default['check_wordster']['database']['mysql_service_name'] = 'default'
 default['check_wordster']['database']['root_password'] = 'changeme'
 default['check_wordster']['database']['data_dir'] = '/data'
 default['check_wordster']['database']['port'] = '3306'
